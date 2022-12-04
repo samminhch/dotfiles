75f780f8feed7bc26b287b298038f5638ca9aa31
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2022-12-04 00:53:13 -0600

    added alias for ls to lsd

--- vars.sh
+++ vars.sh
@@ -11,14 +11,19 @@ alias gitu='git add . && git commit && git push'
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
+    if command -v lsd > /dev/null;
+    then
+        alias ls='lsd '
+    else
+        test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+        alias ls='ls --color=auto'
+        alias dir='dir --color=auto'
+        alias vdir='vdir --color=auto'
 
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+        alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+        alias egrep='egrep --color=auto'
+    fi
 fi
 
 # add ~/.local/bin to PATH
