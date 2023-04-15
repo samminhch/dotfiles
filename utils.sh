cc904aefbaf9678bd57b9d0d6f1deab4a9c152fa
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2023-04-15 12:40:24 -0500

    Updated yadm bootstrap to be more verbose,         made commands in utils.sh more verbose,         added a nvims command in utils.sh to switch         between neovim configurations when needed

--- utils.sh
+++ utils.sh
@@ -1,3 +1,23 @@
+print_error()
+{
+    local RED="\033[31m"
+    local RED_BOLD="\033[1;31m"
+    local RESET="\033[0m"
+
+    printf "%b[ERROR]%b " "$RED_BOLD" "$RED"
+    printf "%s%b\n" "$1" "$RESET"
+}
+
+print_okay()
+{
+    local GREEN="\033[32m"
+    local GREEN_BOLD="\033[1;32m"
+    local RESET="\033[0m"
+
+    printf "%b[OKAY]%b " "$GREEN_BOLD" "$GREEN"
+    printf "%s%b\n" "$1" "$RESET"
+}
+
 req_link()
 {
     read -p "Do you wish to sync '$1' to '$2' [y/N]? " resp
@@ -9,8 +29,9 @@ req_link()
         fi
 
         ln -s $2 $1
+        print_okay "$1 was linked to $2"
     else
-        echo "$1 was not linked to $2."
+        print_error "$1 was not linked to $2"
     fi
 }
 
@@ -32,15 +53,73 @@ ex()
             *.zip)       unzip $1     ;;
             *.Z)         uncompress $1;;
             *.7z)        7z x $1      ;;
-            *)           echo "'$1' cannot be extracted via ex()" ;;
+            *)           print_error "'$1' cannot be extracted via ex()" ;;
         esac
     else
-        echo "'$1' is not a valid file"
+        print_error "'$1' is not a valid file"
     fi
 }
 
 # from https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 command_exists() 
 {
-  command -v "$1" >/dev/null 2>&1
+    for cmd_name in "$@"; do
+        command -v "$cmd_name" >/dev/null
+        cmd_status=$?
+
+        if [ "$cmd_status" -eq 0 ];
+        then
+            print_okay "$cmd_name is an executable"
+        else
+            print_error "$cmd_name isn't an executable"
+        fi
+
+        overall_status=$(( overall_status > cmd_status ? overall_status : cmd_status ))
+    done
+
+    return $overall_status
+}
+
+get_loc()
+{
+    if [ -f /bin/curl ]
+    then
+        curl https://ipinfo.io/$(curl -s https://ipinfo.io/ip)
+    fi
+}
+
+nvims()
+{
+    # check if these commands exists
+    if command_exists nvim fzf grep;
+    then
+        return 1
+    fi
+
+    # check if the correct neovim version is installed
+    local required_version=0.9
+    local nvim_version=$(nvim --version | grep -oP 'NVIM v\K([0-9]+\.[0-9]+)')
+    
+    if (( $(echo "$nvim_version >= $required_version" | bc -l) ));
+    then
+        print_okay "NeoVim version >= $required_version"
+    else
+        print_error "Neovim version is not $required_version. Found version: $nvim_version"
+    fi
+
+
+    # add your neovim configuration names here
+    items=("🐸")
+    config=$(printf "$s\n" "${items[@]}" | fzf --prompt="im config  " --height=50% --layout=reverse --border --exit-0)
+
+    if [[ -z $config ]];
+    then
+        echo "Nothing selected"
+        return 0
+    elif [[ $config == "default" ]]
+    then
+        config=""
+    fi
+
+    NVIM_APPNAME=$config nvim $@
 }
