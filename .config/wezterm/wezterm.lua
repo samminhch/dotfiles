2325f5271e88300c32b5ad16f1383bf0a0c25c39
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2023-09-30 17:58:26 -0500

    updated wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,13 +7,17 @@ return {
             args = { 'btop' }
         },
         {
-            label = 'SSH into oldsoul',
-            args = { 'ssh', 'oldsoul@192.168.1.26' }
-        },
-        {
-            label = 'SSH into minecraft',
-            args = { 'ssh', 'minecraft@192.168.1.26' }
-        },
+            label = 'SSH into CasaOS',
+            args = { 'ssh', 'casaos@192.168.1.4' }
+        }
+        -- {
+        --     label = 'SSH into oldsoul',
+        --     args = { 'ssh', 'oldsoul@192.168.1.26' }
+        -- },
+        -- {
+        --     label = 'SSH into minecraft',
+        --     args = { 'ssh', 'minecraft@192.168.1.26' }
+        -- },
     },
     keys = {
         { key = 'n', mods = 'CTRL|ALT', action = wezterm.action.ShowLauncher },
