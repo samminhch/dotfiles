b491b9aaef51eb92e00924a3913d304251b6d740
samminhch@stemtop <45446622+samminhch@users.noreply.github.com>
2024-02-26 17:37:59 -0600

    updated hyprland and desktop env configs

--- .config/hypr/scripts/idle.sh
+++ .config/hypr/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-swayidle timeout 150 /usr/bin/swaylock \
+swayidle timeout 150 '/usr/bin/gtklock -d -i' \
     timeout 300 'hyprctl "dispatch dpms off"' \
     timeout 360 'systemctl suspend' \
     resume 'hyprctl "dispatch dpms on"' \
-    before-sleep /usr/bin/swaylock
+    before-sleep '/usr/bin/gtklock -d -i'
