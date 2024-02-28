6f3997d87ad5cfb373b47a911aff299b1af3f584
samminhch@stemtop <45446622+samminhch@users.noreply.github.com>
2024-02-28 11:03:15 -0600

    removed extra gtk-lock process when system suspends

--- .config/hypr/scripts/idle.sh
+++ .config/hypr/scripts/idle.sh
@@ -3,5 +3,4 @@
 swayidle timeout 150 '/usr/bin/gtklock -d -i' \
     timeout 300 'hyprctl "dispatch dpms off"' \
     timeout 360 'systemctl suspend' \
-    resume 'hyprctl "dispatch dpms on"' \
-    before-sleep '/usr/bin/gtklock -d -i'
+    resume 'hyprctl "dispatch dpms on"'
